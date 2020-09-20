51388b7948a49794c24f0586512a1af73a03f46c
Bijay Chaudhary <bijaychaudhary@Bijays-MacBook-Pro.local>
2020-09-20 11:16:31 +0545

    Added build of frontend

---
+++ client/build/precache-manifest.c9b4941b1b31994213086205489b4020.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29671ea03647596933ead8b35e2602d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e6e16649f4de75f3007",
+    "url": "/static/css/main.48c78ecf.chunk.css"
+  },
+  {
+    "revision": "108a66a2b40a46e65428",
+    "url": "/static/js/2.b21af452.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b21af452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e6e16649f4de75f3007",
+    "url": "/static/js/main.8337c5e6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "bffeede151350f3a554427c7b64c57e1",
+    "url": "/static/media/boiling-head-logo.bffeede1.png"
+  }
+]);
